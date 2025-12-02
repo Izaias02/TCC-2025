b7ba19d46fd92d134588ce043f1334dc2f3ffd4d
Izaias Santos <izaiasantos20302@gmail.com>
2025-12-01 22:25:02 -0300

    Atualiza conexão com PlanetScale e variáveis de ambiente

--- test-planet.js
+++ test-planet.js
@@ -1,16 +1,11 @@
-// test-planet.js
 require('dotenv').config();
-const db = require('./db');
+const db = require('./db'); // seu db.js
 
-async function testConnection() {
+(async () => {
   try {
     const [rows] = await db.query('SELECT NOW() AS now');
     console.log('Conexão bem-sucedida! Hora do servidor:', rows[0].now);
-    process.exit(0);
   } catch (err) {
     console.error('Erro ao conectar ao PlanetScale:', err.message);
-    process.exit(1);
   }
-}
-
-testConnection();
+})();
