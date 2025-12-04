0bd6818142d5d0bf540678da428f6381e7b9d808
Izaias Santos <izaiasantos20302@gmail.com>
2025-12-03 21:50:54 -0300

    mensagem do commit

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config"; // já deve estar presente
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
